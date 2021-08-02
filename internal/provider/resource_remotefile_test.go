2b19c9c69b19d89e00129485f9741e0438174956
Amund Tenstad <github@amund.io>
2021-08-02 14:15:03 +0200

    Delete 8022 test case

--- internal/provider/resource_remotefile_test.go
+++ internal/provider/resource_remotefile_test.go
@@ -13,44 +13,26 @@ func TestAccResourceRemotefile(t *testing.T) {
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-				resource "remotefile" "foo" {
-				  conn {
-					  host = "remotehost"
-					  username = "root"
-					  sudo = true
-					  password = "password"
-				  }
-				  path = "/tmp/foo.txt"
-				  content = "bar"
-				  permissions = "0777"
-				}
-				`,
+				Config: testAccResourceRemotefile,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr(
 						"remotefile.foo", "content", regexp.MustCompile("bar")),
 				),
 			},
-			{
-				Config: `
-				resource "remotefile" "bar" {
-				  conn {
-					  host = "remotehost8022"
-					  port = 8022
-					  private_key_path = "../../tests/key"
-					  username = "root"
-					  sudo = true
-				  }
-				  path = "/tmp/bar.txt"
-				  content = "bar"
-				  permissions = "0777"
-				}
-				`,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestMatchResourceAttr(
-						"remotefile.bar", "content", regexp.MustCompile("bar")),
-				),
-			},
 		},
 	})
 }
+
+const testAccResourceRemotefile = `
+resource "remotefile" "foo" {
+  conn {
+	  host = "remotehost"
+	  username = "root"
+	  sudo = true
+	  password = "password"
+  }
+  path = "/tmp/foo.txt"
+  content = "bar"
+  permissions = "0777"
+}
+`
