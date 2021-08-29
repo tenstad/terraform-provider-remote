f8064d6949fe8c113f6b4ab5dab72fce36768f8a
Amund Tenstad <github@amund.io>
2021-08-29 13:45:52 +0200

    Test overriding connection

--- internal/provider/data_source_remote_file_test.go
+++ internal/provider/data_source_remote_file_test.go
@@ -1,12 +1,10 @@
 package provider
 
 import (
-	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"golang.org/x/crypto/ssh"
 )
 
 func TestAccDataSourceRemoteFile(t *testing.T) {
@@ -14,31 +12,7 @@ func TestAccDataSourceRemoteFile(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
-			sshClient, err := ssh.Dial("tcp", "remotehost:22", &ssh.ClientConfig{
-				User:            "root",
-				HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-				Auth:            []ssh.AuthMethod{ssh.Password("password")},
-			})
-			if err != nil {
-				panic(err)
-			}
-			session, err := sshClient.NewSession()
-			if err != nil {
-				panic(err)
-			}
-
-			defer session.Close()
-
-			stdin, err := session.StdinPipe()
-			if err != nil {
-				panic(err)
-			}
-			go func() {
-				stdin.Write([]byte("file-content"))
-				stdin.Close()
-			}()
-
-			session.Run(fmt.Sprintf("cat /dev/stdin | tee %s", "/tmp/bar.txt"))
+			writeFileToHost("remotehost:22", "/tmp/bar.txt", "file-content")
 		},
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
@@ -53,10 +27,43 @@ func TestAccDataSourceRemoteFile(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceRemoteFileOverridingDefaultConnection(t *testing.T) {
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			writeFileToHost("remotehost2:22", "/tmp/bar.txt", "file-content")
+		},
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceRemoteFileOverridingDefaultConnection,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(
+						"data.remote_file.baz", "content", regexp.MustCompile("file-content")),
+				),
+			},
+		},
+	})
+}
+
 const testAccDataSourceRemoteFile = `
 data "remote_file" "bar" {
 	conn {
 		host = "remotehost"
+		user = "root"
+		private_key_path = "../../tests/key"
+	}
+	path = "/tmp/bar.txt"
+}
+`
+
+const testAccDataSourceRemoteFileOverridingDefaultConnection = `
+data "remote_file" "baz" {
+	provider = remotehost
+
+	conn {
+		host = "remotehost2"
 		user = "root"
 		private_key_path = "../../tests/key"
 	}
