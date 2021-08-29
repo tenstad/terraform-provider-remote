c662e7db91833e69f86ef67408f51ce313e7932d
Amund Tenstad <github@amund.io>
2021-08-29 08:17:48 +0200

    Fix data source test

--- internal/provider/data_source_remote_file_test.go
+++ internal/provider/data_source_remote_file_test.go
@@ -1,24 +1,52 @@
 package provider
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"golang.org/x/crypto/ssh"
 )
 
 func TestAccDataSourceRemoteFile(t *testing.T) {
 
 	resource.UnitTest(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
+		PreCheck: func() {
+			testAccPreCheck(t)
+			sshClient, err := ssh.Dial("tcp", "remotehost:22", &ssh.ClientConfig{
+				User:            "root",
+				HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+				Auth:            []ssh.AuthMethod{ssh.Password("password")},
+			})
+			if err != nil {
+				panic(err)
+			}
+			session, err := sshClient.NewSession()
+			if err != nil {
+				panic(err)
+			}
+
+			defer session.Close()
+
+			stdin, err := session.StdinPipe()
+			if err != nil {
+				panic(err)
+			}
+			go func() {
+				stdin.Write([]byte("file-content"))
+				stdin.Close()
+			}()
+
+			session.Run(fmt.Sprintf("cat /dev/stdin | tee %s", "/tmp/bar.txt"))
+		},
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataSourceRemoteFile,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr(
-						// TODO: check content is correct
-						"data.remote_file.bar", "content", regexp.MustCompile("")),
+						"data.remote_file.bar", "content", regexp.MustCompile("file-content")),
 				),
 			},
 		},
