6f88faadbb8cb72ebed196d4c5f3534f0524846b
Amund Tenstad <github@amund.io>
2022-02-03 13:43:57 +0100

    Test remote_file ssh agent (#40)
    * Test remote_file ssh agent
    
    * Skip agent test in pipeline

--- internal/provider/resource_remote_file_test.go
+++ internal/provider/resource_remote_file_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"os"
 	"regexp"
 	"testing"
 
@@ -52,6 +53,36 @@ func TestAccResourceRemoteFileWithDefaultConnection(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr(
 						"remote_file.resource_2", "content", regexp.MustCompile("resource_2")),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceRemoteFileWithAgent(t *testing.T) {
+	if os.Getenv("SKIP_TEST_AGENT") == "1" {
+		return
+	}
+
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				resource "remote_file" "resource_3" {
+					conn {
+						host = "remotehost"
+						user = "root"
+						agent = true
+					}
+					path = "/tmp/resource_3.txt"
+					content = "resource_3"
+				  }
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(
+						"remote_file.resource_3", "content", regexp.MustCompile("resource_3")),
 				),
 			},
 		},
