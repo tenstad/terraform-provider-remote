858cc9c1f6995a02e4639ae5a90c59922710b1b5
Amund Tenstad <github@amund.io>
2022-02-04 18:39:23 +0100

    Create playground (#43)

---
+++ playground/.terraform.lock.hcl
@@ -0,0 +1,4 @@
+provider "registry.terraform.io/tenstad/remote" {
+  version     = "99.0.0"
+  constraints = "99.0.0"
+}
---
+++ playground/provider.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    remote = {
+      source  = "tenstad/remote"
+      version = "99.0.0"
+    }
+  }
+}
