07a6cbb4ca50e48467bf6666bd723fc2714e8a04
Amund Tenstad <github@amund.io>
2024-10-19 11:05:24 +0200

    chore: allow running tests directly when developing inside a container

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,13 @@
-mkdir ~/.ssh
-cp tests/key ~/.ssh
-chmod 600 ~/.ssh/key
+#!/bin/sh
 
-eval $(ssh-agent)
-ssh-add ~/.ssh/key
+eval "$(ssh-agent)"
+chmod 0600 tests/key
+ssh-add tests/key
+
+ssh-keygen -f ~/.ssh/known_hosts -R remotehost
+ssh-keygen -f ~/.ssh/known_hosts -R remotehost2
 
 export SKIP_TEST_MOVE_FILE_BY_MODIFYING_PROVIDER=1
 
+# shellcheck disable=SC2086
 go test ./... -v $TESTARGS -timeout 120m
